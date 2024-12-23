dcadec3828194e4dc66043bed5e1e84c69d50c5b
GithubCosmin <cosmin.posirca@yahoo.com>
2024-12-23 20:52:34 +0200

    fix-db

--- start-db.sh
+++ start-db.sh
@@ -4,6 +4,6 @@ export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 export MSYS_NO_PATHCONV=1
 docker rm -f scylla || true
-docker run --volume "$PWD/.db:/var/lib/scylla" --name scylla -p 0.0.0.0:9042:9042 -d scylladb/scylla:6.2
+docker run --volume "scylla-db:/var/lib/scylla" --name scylla -p 0.0.0.0:9042:9042 -d scylladb/scylla:6.2
 
 
